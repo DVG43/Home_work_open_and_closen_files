5dc1dc73c35cac79b7f2b8ab5f86011572f99a26
Galuta Dmitrii <dvgaluta@gmail.com>
2021-12-18 16:49:32 +0300

    Commit test

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
+import os
+# cook_book = {
+#   'Омлет': [
+#     {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+#     {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+#     {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+#     ],
+#   'Утка по-пекински': [
+#     {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+#     {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+#     {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+#     {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+#     ],
+#   'Запеченный картофель': [
+#     {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+#     {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+#     {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+#     ]
+#   }
+# my_file = open("BabyFile.txt", "w+")
+# my_file.write("Привет, файл!")
+# my_file.close()
+
+# my_file = open ("coock_book.txt","w+")
+# my_file.write("Все ОК")
+# my_file.close()
+
+my_file = open ("coock_book.txt",'rt')
+print(my_file.read())
+my_file.close()
+
+# with open('start_file.txt', encoding = "utf-8") as file:
+#       # encoding = "utf-8"
+#       print(file.read())
+
+
+
 
